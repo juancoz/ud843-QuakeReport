4ccc2ed9e6723a1f47050a5e23b545eef1cab878
Juan Carlos Rodriguez Polania <juancoz@hotmail.com>
2020-10-17 18:59:00 -0500

    1.2 Parse JSON response to create earthquakes list

--- app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -42,7 +42,7 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
         View listItemView = convertView;
         if (listItemView == null) {
             listItemView = LayoutInflater.from(getContext()).inflate(
-                    R.layout.list_item, parent, false);
+                    R.layout.earthquake_list_item, parent, false);
         }
 
         // Find the earthquake at the given position in the list of earthquakes
